43e04b70cfe16f067216a3395090bef75a022ebc
sonohoshi <yd111544@gmail.com>
2021-07-24 19:20:29 +0900

    Make SpriteOutline shader
    and components

---
+++ Assets/_Scripts/SpriteOutlineRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode][RequireComponent(typeof(SpriteRenderer))]
+public class SpriteOutlineRenderer : MonoBehaviour
+{
+    public Color color = Color.white;
+
+    [Range(0, 16)][SerializeField]
+    int outlineSize = 1;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void OnEnable()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateOutline(true);
+    }
+
+    private void OnDisable()
+    {
+        UpdateOutline(false);
+    }
+
+    private void Update()
+    {
+        UpdateOutline(true);
+    }
+
+    private void UpdateOutline(bool outline)
+    {
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+        spriteRenderer.GetPropertyBlock(mpb);
+        mpb.SetFloat("_Outline", outline ? 1f : 0f);
+        mpb.SetColor("_OutlineColor", color);
+        mpb.SetFloat("_OutlineSize", outlineSize);
+        spriteRenderer.SetPropertyBlock(mpb);
+    }
+}
